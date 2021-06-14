3ac958cec0bdee7c5373c1ff692ac46a05e4f501
prathamsm7 <79472183+prathamsm7@users.noreply.github.com>
2021-06-14 15:35:55 +0530

    Update cloudinary.js

--- cloudinary.js
+++ cloudinary.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "dqm9cemhk",
-  api_key: "273768889229629",
-  api_secret: "WeS5btOPHc-R3rapzpXJzA4aLlE",
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
 
 // console.log(process.env.CLOUDINARY_CLOUD_NAME);
